7f77706bc93d4003bf8d68d1fcd47719d1b8a555
wanghao1993 <whao53333@gmail.com>
2022-03-01 23:13:58 +0800

    feat: docs

---
+++ .vuepress/config.js
@@ -0,0 +1,104 @@
+module.exports = {
+  title: "天上的月亮",
+  description: 'About my thoughts and blog',
+  dest: 'public',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  theme: 'reco',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/', icon: 'reco-home' },
+      { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
+      // { text: 'Docs', 
+      //   icon: 'reco-message',
+      //   items: [
+      //     { text: 'vuepress-reco', link: '/docs/theme-reco/' }
+      //   ]
+      // },
+      { text: 'Contact', 
+        icon: 'reco-message',
+        items: [
+          { text: 'GitHub', link: 'https://github.com/wanghao1993', icon: 'reco-github' },
+          { text: '掘金', link: 'https://juejin.cn/user/3227821868060695', icon: 'reco-github' }
+        ]
+      }
+    ],
+    sidebar: {
+      '/docs/theme-reco/': [
+        '',
+        'theme',
+        'plugin',
+        'api'
+      ],
+      '/docs/interview/': [
+        '',
+        'summary'
+      ]
+    },  
+    type: 'blog',
+    // 博客设置
+    blogConfig: {
+      // category: {
+      //   location: 2, // 在导航栏菜单中所占的位置，默认2
+      //   text: 'Category' // 默认 “分类”
+      // },
+      tag: {
+        location: 2, // 在导航栏菜单中所占的位置，默认3
+        text: '标签' // 默认 “标签”
+      }
+    },
+    friendLink: [
+      {
+        title: '蜉蝣撼树',
+        desc: '不积硅步，无以至千里',
+        email: '2682265436@qq.com',
+        link: 'https://super-super.cn/'
+      },
+      {
+        title: '公众号：前端工兵',
+        // desc: 'A simple and beautiful vuepress Blog & Doc theme.',
+        avatar: "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        link: 'https://vuepress-theme-reco.recoluan.com'
+      },
+    ],
+    logo: '/logo.png',
+    // 搜索设置
+    search: true,
+    searchMaxSuggestions: 10,
+    // 自动形成侧边导航
+    // sidebar: 'auto',
+    // 最后更新时间
+    lastUpdated: 'Last Updated',
+    // 作者
+    author: 'isaac Wang',
+    // 作者头像
+    authorAvatar: '/logo.png',
+    // 备案号
+    record: '粤ICP备2022001810号',
+    // 项目开始时间
+    startYear: '2022'
+    /**
+     * 密钥 (if your blog is private)
+     */
+
+    // keyPage: {
+    //   keys: ['your password'],
+    //   color: '#42b983',
+    //   lineColor: '#42b983'
+    // },
+
+    /**
+     * valine 设置 (if you need valine comment )
+     */
+
+    // valineConfig: {
+    //   appId: '...',// your appId
+    //   appKey: '...', // your appKey
+    // }
+  },
+  markdown: {
+    lineNumbers: true
+  }
+}  
--- docs/.vitepress/theme/index.js
+++
@@ -1,9 +0,0 @@
-// .vitepress/theme/index.js
-import DefaultTheme from 'vitepress/theme'
-import MyLayout from './myLayout.vue'
-
-export default {
-  ...DefaultTheme,
-  // override the Layout with a wrapper component that injects the slots
-  Layout: MyLayout
-}
